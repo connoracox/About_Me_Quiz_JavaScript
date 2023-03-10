ed4eb96d5d08b6697ff0c3c4a4f07d71820f74ee
Connor Cox <connor.a.cox@gmail.com>
2023-03-10 10:37:18 -0500

    fixed a few bugs in color adding

--- main.js
+++ main.js
@@ -14,43 +14,73 @@ function checkAnswers(){
 
     if(question1 == "The Shawshank Redemption") {
         count++
-        document.myQuiz.q1.style.backgroundColor = 'lightgreen !important'
+        document.myQuiz.q1.style.backgroundColor = 'lightgreen'
+    }
+    else {
+        document.myQuiz.q1.style.backgroundColor = 'red'
     }
     if(question2 =="Into Thin Air") {
         count++
-        document.myQuiz.q2.style.backgroundColor = 'lightgreen !important'
+        document.myQuiz.q2.style.backgroundColor = 'lightgreen'
+    }
+    else {
+        document.myQuiz.q1.style.backgroundColor = 'red'
     }
     if(question3 == "yellow") {
         count++
-        document.myQuiz.q3.style.backgroundColor = 'lightgreen !important'
+        document.myQuiz.q3.style.backgroundColor = 'lightgreen'
+    }
+    else {
+        document.myQuiz.q1.style.backgroundColor = 'red'
     }
     if(question4 == "climbing") {
         count++
-        document.myQuiz.q4.style.backgroundColor = 'lightgreen !important'
+        document.myQuiz.q4.style.backgroundColor = 'lightgreen'
+    }
+    else {
+        document.myQuiz.q1.style.backgroundColor = 'red'
     }
     if(question5 == "tacos") {
         count++
-        document.myQuiz.q5.style.backgroundColor = 'lightgreen !important'
+        document.myQuiz.q5.style.backgroundColor = 'lightgreen'
+    }
+    else {
+        document.myQuiz.q1.style.backgroundColor = 'red'
     }
     if(question6 == "fiddleleaf fig") {
         count++
-        document.myQuiz.q6.style.backgroundColor = 'lightgreen !important'
+        document.myQuiz.q6.style.backgroundColor = 'lightgreen'
+    }
+    else {
+        document.myQuiz.q1.style.backgroundColor = 'red'
     }
     if(question7 == "Texas") {
         count++
-        document.myQuiz.q7.style.backgroundColor = 'lightgreen !important'
+        document.myQuiz.q7.style.backgroundColor = 'lightgreen'
+    }
+    else {
+        document.myQuiz.q1.style.backgroundColor = 'red'
     }
     if(question8 == "Washington") {
         count++
-        document.myQuiz.q8.style.backgroundColor = 'lightgreen !important'
+        document.myQuiz.q8.style.backgroundColor = 'lightgreen'
+    }
+    else {
+        document.myQuiz.q1.style.backgroundColor = 'red'
     }
     if(question9 == "Florida") {
         count++
-        document.myQuiz.q9.style.backgroundColor = 'lightgreen !important'
+        document.myQuiz.q9.style.backgroundColor = 'lightgreen'
+    }
+    else {
+        document.myQuiz.q1.style.backgroundColor = 'red'
     }
     if(question10 == "Glimmer") {
         count++
-        document.myQuiz.q10.style.backgroundColor = 'lightgreen !important'
+        document.myQuiz.q10.style.backgroundColor = 'lightgreen'
+    }
+    else {
+        document.myQuiz.q1.style.backgroundColor = 'red'
     }
 
     document.getElementById("afterSubmit").style.visibility = "visible"
